5da8b5ed2a21d9084566acb5deab9b7e9b2fd155
Kaveh Mossavati <kaveh.mossavati@gmail.com>
2020-05-08 13:18:45 +0200

    Implemenetd initial image file uploading system via carrierwave for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -75,7 +75,9 @@ private
 def portfolio_params
   params.require(:portfolio).permit(:title, 
                                     :substitle, 
-                                    :body, 
+                                    :body,
+                                    :main_image,
+                                    :thumb_image, 
                                     technologies_attributes: [:name])
 end
 
